60871db38a0398e47735138dfc9d4d063bbdf9a9
natali <NataliDegtyreva@yandex.ru>
2023-06-14 11:37:54 +0300

    add files

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useState } from "react";
+import logo from "../images/logo.svg";
+import { AppContext } from "../contexts/AppContext";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+export const Header = () => {
+  const context = useContext(AppContext);
+  const location = useLocation();
+
+  const setLocation = () => {
+    return !!(location.pathname === "/sign-in" || location.pathname === "/");
+  };
+
+  const [isLogging, setIsLogging] = useState(setLocation);
+
+  useEffect(() => {
+    setIsLogging(setLocation);
+  });
+
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Логотип Mesto" />
+      {isLogging && !context.isLoggedIn && (
+        <Link to="/sign-up" className="link header__link">
+          Зарегестрироваться
+        </Link>
+      )}
+
+      {!isLogging && !context.isLoggedIn && (
+        <Link to="/sign-in" className="link header__link">
+          Войти
+        </Link>
+      )}
+
+      {context.isLoggedIn && (
+        <Link
+          to="/sign-in"
+          className="link header__link"
+          onClick={context.handleSignOut}
+        >
+          Выйти
+        </Link>
+      )}
+    </header>
+  );
+};
---
+++ src/utils/api.js
@@ -0,0 +1,83 @@
+import { apiConfig } from './constants';
+
+class Api {
+  constructor(config) {
+    this.url = `${config.url}/${config.cohort}`;
+    this.headers = config.headers;
+  }
+
+  loadUserInfo() {
+    return fetch(`${this.url}/users/me`, {
+      headers: this.headers,
+    }).then(this._handleResult);
+  }
+
+  editUserInfo(userInfo) {
+    return fetch(`${this.url}/users/me`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify(userInfo),
+    }).then(this._handleResult);
+  }
+
+  editUserAvatar(avatar) {
+    return fetch(`${this.url}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify(avatar),
+    }).then(this._handleResult);
+  }
+
+  getInitialCards() {
+    return fetch(`${this.url}/cards`, {
+      headers: this.headers,
+    }).then(this._handleResult);
+  }
+
+  createCard(cardParams) {
+    return fetch(`${this.url}/cards`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify(cardParams),
+    }).then(this._handleResult);
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this.url}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    }).then(this._handleResult);
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    if (isLiked) {
+      return this.likeCard(cardId);
+    }
+    return this.dislikeCard(cardId);
+  }
+
+  likeCard(cardId) {
+    return fetch(`${this.url}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this.headers,
+    }).then(this._handleResult);
+  }
+
+  dislikeCard(cardId) {
+    return fetch(`${this.url}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this.headers,
+    }).then(this._handleResult);
+  }
+
+  _handleResult(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(`Error: ${response?.message || response?.status}`);
+  }
+}
+
+const api = new Api(apiConfig);
+
+export { api };
